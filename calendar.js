975eae2e8181dbe26e7713ab1cfb71d8373dca24
Gifsta <96801283+Gifsta@users.noreply.github.com>
2024-07-04 11:27:30 +0200

    init

---
+++ calendar.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    let newData = [];
+
+    fetch("json/date")
+        .then(res => res.json())
+        .then(data => {
+
+            for (let i = 0; i < data.length; i++) {
+                newData.push({ 
+                    title: data[i].Name, 
+                    start: data[i].DateTime, 
+                    
+                    display: 'block',
+                    borderColor: "pink", 
+                    backgroundColor: "black" })
+            }
+            let calendarEl = document.getElementById('calendar');
+            let calendar = new FullCalendar.Calendar(calendarEl, {
+                initialView: 'dayGridMonth',
+                progressiveEventRendering: true,
+                events: newData,
+                views: {
+                    timeGrid: {
+                        dayMaxEventRows: 1
+                    }
+                }
+            });
+            calendar.render();
+            calendar.setOption('locale', 'fr');
+        })
+
+});
